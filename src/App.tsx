0321ada42249d2b54a3ad860420153127d67f7fa
Ron Jean-Francois <rjfrancoisjr@icloud.com>
2023-02-05 13:06:32 -0500

    update layout and add footer

--- src/App.tsx
+++ src/App.tsx
@@ -26,28 +26,39 @@ const App: Component = () => {
 				<h1>
 					BMI <span class={styles.text_gradient}>CALCULATOR</span>
 				</h1>
-				<p class={styles.instructions}>
-					The Body Mass Index (BMI) is a measurement of body fat using height
-					and weight. To calculate BMI you would divide the body mass by the
-					square of the body height.
-					<br />
-					<strong>Metric BMI Equation:</strong>
-					<br />
-					<code>weight (kg) / [height (m)] ** 2</code> OR
-					<br />
-					<code>[weight (kg) / height (cm) / height (cm)] * 10,000</code>
-					<br />
-					<strong>Imperial/English BMI Equation:</strong>
-					<br />
-					<code>weight (lb) / [height (in)] ** 2 * 703</code>
-					<br />
-					Or you can use the calculator below.
-				</p>
+				<section class={styles.instructions}>
+					<p>
+						The Body Mass Index (BMI) is a measurement of body fat using height
+						and weight. To calculate BMI you would divide the body mass by the
+						square of the body height.
+						<br />
+						<strong>Metric BMI Equation:</strong>
+						<br />
+						<code>weight (kg) / [height (m)] ** 2</code> OR
+						<br />
+						<code>[weight (kg) / height (cm) / height (cm)] * 10,000</code>
+						<br />
+						<strong>Imperial/English BMI Equation:</strong>
+						<br />
+						<code>weight (lb) / [height (in)] ** 2 * 703</code>
+						<br />
+						Or you can use the calculator below.
+					</p>
+				</section>
 				<section class={styles.bmi_grid}>
 					<AdultBMICalculator />
 					<Table />
 				</section>
 			</main>
+			<footer>
+				<div class={styles.footer_bar}>
+					<div class={styles.blue_bar}></div>
+					<span>
+						© 2023 Website Designed and Developed By:{" "}
+						<a href="https://ronjeanfrancois.com">Ron Jean-Francois</a>
+					</span>
+				</div>
+			</footer>
 		</>
 	)
 }
